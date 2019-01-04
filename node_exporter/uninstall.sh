965b97fb622f4dc116f9ed953c66af2d3d13dd62
Roland Bapst <roland.bapst@tamedia.ch>
2019-01-04 13:46:25 +0100

    node_exporter...

--- node_exporter/uninstall.sh
+++ node_exporter/uninstall.sh
@@ -6,10 +6,11 @@ PROGRAM=node_exporter
 RELEASE=${NODE_EXPORTER_RELEASE:-0.17.0}
 DIR=$PROGRAM-$RELEASE.linux-amd64
 
+service $PROGRAM stop
+chkconfig --del $PROGRAM
+
 [ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -h /opt/$PROGRAM ] && rm -f /opt/$PROGRAM
 
-service $PROGRAM stop
-chkconfig --del $PROGRAM
 [ -f /usr/bin/uninstall-$PROGRAM.sh ] && rm -f /usr/bin/uninstall-$PROGRAM.sh
 exit 0
