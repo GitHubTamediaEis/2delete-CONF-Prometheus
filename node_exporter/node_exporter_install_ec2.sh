433a0d1bb62b09c0b167349bbc41190c372f8b41
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-01-25 08:24:17 +0100

    Devel sh

--- node_exporter/node_exporter_install_ec2.sh
+++ node_exporter/node_exporter_install_ec2.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 # Installation script of Prometheus's node exporter usabla by any EC2 Linux
-# Will by installed in /opt/node_exporter--amd64
+# install v1.0 25.01.2019
 
 # Define release of prometheus and deduce installation directory
 PROGRAM=node_exporter
-##RELEASE=${NODE_EXPORTER_RELEASE:-0.17.0}
 INSTALLDIR="/tmp/node_exporter_installation"
-##CURDIR=$(dirname $0)
 URL_DL_GITHUB="wget https://github.com/GitHubTamediaEis/CONF-Prometheus/raw/v0.3/node_exporter/"
 FILETODL="install.sh start_stop_node_exporter.sh uninstall.sh"
+INSTALLSH="install.sh"
 
-[ ! -d $INSTALLDIR ] && mkdir $INSTALLDIR 
+# if install dir exist --> remove it
+[ -d $INSTALLDIR ] && rm -rf $INSTALLDIR
+
+# Create install dir
+[ ! -d $INSTALLDIR ] && mkdir $INSTALLDIR
+
+# Download 'package'
 cd $INSTALLDIR
-pwd
-ls -l
-for shfile in 'FILETODL'
+
+for shfile in $FILETODL
 do
-    echo $shfile
     urldl=$URL_DL_GITHUB$shfile
+    wget $urldl
+    chmod 755 $shfile
+
 done
+
+# Install 'package'
+./$INSTALLSH
+
+# remove installation dir
+cd ..
+[ -d $INSTALLDIR ] && [ -f $INSTALLDIR/$INSTALLSH ] && echo "OK RM" &&  rm -rf $INSTALLDIR
+
