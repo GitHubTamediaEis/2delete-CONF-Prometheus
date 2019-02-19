8f428f0bf52e4ff7bdfce7f4626c8a3700bebfb0
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-02-19 18:23:51 +0100

    Fix small bug

--- prometheus/uninstall.sh
+++ prometheus/uninstall.sh
@@ -12,7 +12,7 @@ chkconfig --del prometheus
 [ -h /opt/prometheus ] && rm -f /opt/prometheus
 [ -d /etc/prometheus ] && rm -rf /etc/prometheus
 
-[ -f /etc/init.d/prometheus] && rm -f /etc/init.d/prometheus
-[ -f /usr/bin/update-prometheus-config.sh] && rm -f /usr/bin/update-prometheus-config.sh
-[ -f /usr/bin/uninstall-prometheus.sh] && rm -f /usr/bin/uninstall-prometheus.sh
+[ -f /etc/init.d/prometheus ] && rm -f /etc/init.d/prometheus
+[ -f /usr/bin/update-prometheus-config.sh ] && rm -f /usr/bin/update-prometheus-config.sh
+[ -f /usr/bin/uninstall-prometheus.sh ] && rm -f /usr/bin/uninstall-prometheus.sh
 exit 0
