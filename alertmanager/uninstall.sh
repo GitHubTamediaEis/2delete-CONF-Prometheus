5e7380f5e2836ba637003c95647e0605509e46b4
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-02-19 07:44:13 +0100

    add uninstall.sh on alertmanager

---
+++ alertmanager/uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# De-installation script of alertmanager.
+
+# Define release of alertmanager and deduce installation directory
+RELEASE=${ALERTMANAGER_RELEASE:-0.16.1}
+DIR=alertmanager-$RELEASE.linux-amd64
+
+service alertmanager stop
+chkconfig --del alertmanager
+
+[ -d /opt/$DIR ] && rm -rf /opt/$DIR
+[ -h /opt/alertmanager ] && rm -f /opt/alertmanager
+[ -f /etc/prometheus/alertmanager.yml ] && rm -rf /etc/prometheus/alertmanager.*
+
+[ -f /etc/init.d/alertmanager] && rm -f /etc/init.d/alertmanager
+[ -f /usr/bin/update-alertmanager-config.sh] && rm -f /usr/bin/update-alertmanager-config.sh
+[ -f /usr/bin/uninstall-alertmanager.sh] && rm -f /usr/bin/uninstall-alertmanager.sh
+exit 0
