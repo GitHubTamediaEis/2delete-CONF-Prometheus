5098f27ef3e720a3e55e4ea08d07317d8e0f8b32
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-02-19 09:09:44 +0100

    Fix bug sh

--- alertmanager/uninstall.sh
+++ alertmanager/uninstall.sh
@@ -10,9 +10,9 @@ chkconfig --del alertmanager
 
 [ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -h /opt/alertmanager ] && rm -f /opt/alertmanager
-[ -f /etc/prometheus/alertmanager.yml ] && rm -rf /etc/prometheus/alertmanager.*
+[ -f /etc/prometheus/alertmanager.yml ] && rm -f /etc/prometheus/alertmanager.yml
 
-[ -f /etc/init.d/alertmanager] && rm -f /etc/init.d/alertmanager
-[ -f /usr/bin/update-alertmanager-config.sh] && rm -f /usr/bin/update-alertmanager-config.sh
-[ -f /usr/bin/uninstall-alertmanager.sh] && rm -f /usr/bin/uninstall-alertmanager.sh
+[ -f /etc/init.d/alertmanager ] && rm -f /etc/init.d/alertmanager
+[ -f /usr/bin/update-alertmanager-config.sh ] && rm -f /usr/bin/update-alertmanager-config.sh
+[ -f /usr/bin/uninstall-alertmanager.sh ] && rm -f /usr/bin/uninstall-alertmanager.sh
 exit 0
