4411fef9949079f5fe851214db15902569746a69
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-03-04 08:56:31 +0100

    add rm yace cfg file

--- yace_cloudwatch_exporter/uninstall.sh
+++ yace_cloudwatch_exporter/uninstall.sh
@@ -5,12 +5,15 @@
 PROGRAM=yace_cloudwatch_exporter
 RELEASE=${YACE_CLOUDWATCH_EXPORTER_RELEASE:-0.12.0}
 DIR=$PROGRAM-$RELEASE
+CFGFILE=/etc/prometheus/$PROGRAM.yml
 
 service $PROGRAM stop
 chkconfig --del $PROGRAM
 
 [ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -h /opt/$PROGRAM ] && rm -f /opt/$PROGRAM
+
+[ -f $CFGFILE ] && rm -f $CFGFILE
 
 [ -f /usr/bin/uninstall-$PROGRAM.sh ] && rm -f /usr/bin/uninstall-$PROGRAM.sh
 exit 0
