b3b75e4ab911b5c0ba4ff808df2b9019e5c05a6a
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-03-04 07:27:16 +0100

    Add yace_cloudw._exp.

---
+++ yace_cloudwatch_exporter/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Installation script of Prometheus's cloudwatch exporter
+# Will by installed in /opt/cloudwatch_exporter-x.y.z-amd64
+# with x.y.z = release
+#https://search.maven.org/remotecontent?filepath=io/prometheus/cloudwatch/cloudwatch_exporter/$RELEAS/cloudwatch_exporter-$RELEASE-jar-with-dependencies.jar
+
+# Define release of cloudwatch_exporter and deduce installation directory and
+# jar file name (cloudwatch_exporter is a java program)
+PROGRAM=cloudwatch_exporter
+RELEASE=${CLOUDWATCH_EXPORTER_RELEASE:-0.5.0}
+DIR=$PROGRAM-$RELEASE
+CFGDIR=/etc/prometheus
+JAR=$PROGRAM-jar-with-dependencies.jar
+
+CURDIR=$(dirname $0)
+
+if [ \! -d /opt/$DIR ]; then
+    mkdir /opt/$DIR
+    URL="https://search.maven.org/remotecontent?filepath=io/prometheus/cloudwatch/cloudwatch_exporter/$RELEASE/$DIR-jar-with-dependencies.jar"
+    wget -O /opt/$DIR/$JAR $URL
+    if [ $? != 0 ]; then
+	echo "Download of $PROGRAM failed"
+	exit 1
+    fi
+fi
+
+# Handle soft link (/opt/$PROGRAM to /opt/$PROGRAM-x.y.z...)
+[ -h /opt/$PROGRAM ] && rm -f /opt/$PROGRAM
+ln -s /opt/$DIR /opt/$PROGRAM
+
+# Put configuration file if not exists
+# Notice that the configuration script should be in the same diretory
+# as this script
+[ -d $CFGDIR ] || mkdir $CFGDIR 
+[ -f $CFGDIR/$PROGRAM.yaml ] || cp $CURDIR/$PROGRAM.yaml $CFGDIR/$PROGRAM.yaml
+
+# Handle start-stop script
+cp $CURDIR/start_stop_$PROGRAM.sh /etc/init.d/$PROGRAM
+chmod +x /etc/init.d/$PROGRAM
+chkconfig --add $PROGRAM
+service $PROGRAM start
+
+# Handle update and uninstall scripts
+cp $CURDIR/uninstall.sh /usr/bin/uninstall-$PROGRAM.sh
+chmod +x /usr/bin/uninstall-$PROGRAM.sh
