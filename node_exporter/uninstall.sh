c64ebf9a4c2cd24fbae8d9fabba5bce936a5ff03
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-03-14 16:42:08 +0100

    Fix bug

--- node_exporter/uninstall.sh
+++ node_exporter/uninstall.sh
@@ -12,7 +12,7 @@ chkconfig --del $PROGRAM
 [ -h /opt/$PROGRAM ] && rm -f /opt/$PROGRAM
 [ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -d /var/lib/node_exporter ] && rm -rf /var/lib/node_exporter
-[ -h /etc/init.d/$PROGRAM ] && rm -f /etc/init.d/$PROGRAM
+[ -f /etc/init.d/$PROGRAM ] && rm -f /etc/init.d/$PROGRAM
 
 [ -f /usr/bin/uninstall-$PROGRAM.sh ] && rm -f /usr/bin/uninstall-$PROGRAM.sh
 exit 0
