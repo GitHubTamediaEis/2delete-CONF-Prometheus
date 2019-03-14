6e65584c851db3c7baa8913d6f2ea4cf2738fa09
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-03-14 16:39:56 +0100

    Add rm /etc/init.d/node_exporter

--- node_exporter/uninstall.sh
+++ node_exporter/uninstall.sh
@@ -9,10 +9,10 @@ DIR=$PROGRAM-$RELEASE.linux-amd64
 service $PROGRAM stop
 chkconfig --del $PROGRAM
 
-[ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -h /opt/$PROGRAM ] && rm -f /opt/$PROGRAM
+[ -d /opt/$DIR ] && rm -rf /opt/$DIR
 [ -d /var/lib/node_exporter ] && rm -rf /var/lib/node_exporter
-
+[ -h /etc/init.d/$PROGRAM ] && rm -f /etc/init.d/$PROGRAM
 
 [ -f /usr/bin/uninstall-$PROGRAM.sh ] && rm -f /usr/bin/uninstall-$PROGRAM.sh
 exit 0
