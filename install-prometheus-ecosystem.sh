88a9c105f833ff1473b66353f140783c3c13c783
LaurentMoix <33905478+LaurentMoix@users.noreply.github.com>
2019-03-18 13:30:18 +0100

    Fix install script type error

--- install-prometheus-ecosystem.sh
+++ install-prometheus-ecosystem.sh
@@ -3,6 +3,6 @@
 [ -f ecosystem-versions.sh ] && . ecosystem-versions.sh
 [ -f prometheus/install.sh ] && bash prometheus/install.sh
 [ -f alertmanager/install.sh ] && bash alertmanager/install.sh
-[ -f node_exporter/install.sh ] && bash node_exporter/install.sh
+[ -f node_exporter/install.sh ] && bash node_exporter/install_update.sh
 [ -f cloudwatch_exporter/install.sh ] && bash cloudwatch_exporter/install.sh
-[ -f yace_cloudwatch_exporter/install.sh ] && bash yace_cloudwatch_exporter/install_update.sh
+[ -f yace_cloudwatch_exporter/install.sh ] && bash yace_cloudwatch_exporter/install.sh
