3a6351533ecff014d49d85315d8e192a71ac2efc
Milija <49436496+milijander88@users.noreply.github.com>
2019-07-29 11:19:27 +0200

    Update alertsnitch.sh

--- alertsnitch/alertsnitch.sh
+++ alertsnitch/alertsnitch.sh
@@ -3,7 +3,7 @@
 # because cfn-hub creates a backup file in the cron.hourly dir
 EcsLoadBalancer=$1
 grep  'alertsnitch' '/etc/prometheus/alertmanager.yml'
-if [[ ${ActivateInstallationAlertsnitch} == 'yes' && $? != 0 ]]; then
+if [[  $? != 0 ]]; then
     sed -i '/^inhibit_rules:.*/i - name: 'alertsnitch'\n  webhook_configs:\n    - url: http://${EcsLoadBalancer}/webhook2' /etc/prometheus/alertmanager.yml
     service alertmanager reload
     exit 0
